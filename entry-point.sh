17910876049a1f665ad71b34ffc82ea4f355c240
Oyudo Gerald <gerald.oyudo@gmail.com>
2017-01-12 14:17:13 +0100

    Update entry-point.sh

--- entry-point.sh
+++ entry-point.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 fileName=${FILE_NAME:-app.jar}
 fileUrl=${FILE_URL:-app.jar}
-cd /var/home/app
+cd /home/app
 if [ ! -f $fileName ]
 then
    wget $fileUrl -O $fileName
 fi
-java -jar $fileName --spring.config.location=/var/home/app/conf/application.yml >> /var/lib/logs/out.txt
+java -jar $fileName --spring.config.location=./conf/application.yml >> /var/lib/logs/out.txt
