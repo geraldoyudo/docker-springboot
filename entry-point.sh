3a0a5044f40249844ee169d7ecc99a256825e445
Oyudo Gerald <gerald.oyudo@gmail.com>
2017-01-12 16:37:03 +0100

    Update entry-point.sh

--- entry-point.sh
+++ entry-point.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 fileName=${FILE_NAME:-app.jar}
 fileUrl=${FILE_URL:-app.jar}
+confName=${CONFIG_FILE_NAME:-application.properties}
+jarOptions=${JAVA_OPTIONS}
+springOptions=${SPRING_OPTIONS}
 cd /home/app
 if [ ! -f $fileName ]
 then
    wget $fileUrl -O $fileName
 fi
-java -jar $fileName --spring.config.location=./conf/application.yml >> /var/lib/logs/out.txt
+java -jar jarOptions $fileName --spring.config.location=./conf/$confName springOptions >> /var/lib/logs/out.txt
